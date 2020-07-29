7a19eabc93cb0f3574d93f05591ae6b51a8c5d28
chenzdido <chenzdido@gmail.com>
2020-07-29 21:32:10 +0800

    [zhe] feat:complete exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,16 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let arr = [];
+  for (let i = start; i !== end;) {
+    arr.push(i);
+    if (start < end) {
+      i++;
+    } else {
+      i--;
+    }
+  }
+  return arr;
 }
 
 export function sum(...numbers) {
@@ -17,6 +25,12 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  let len = numbers.length;
+  let sum=0;
+  for (let i = 0; i < len; i++){
+    sum += numbers[i];
+  }
+  return sum;
 
   throw new Error('Please delete this line and implement the function');
 }
