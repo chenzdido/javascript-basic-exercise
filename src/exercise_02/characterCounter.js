78d09331affb074fdad8ac7a3a4a39d0bbae7e5d
chenzdido <chenzdido@gmail.com>
2020-07-29 21:09:52 +0800

    [zhe] feat:complete exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,22 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (string === undefined || string === null) {
+    return 0;
+  }
+  if (prediction === undefined || prediction === null) {
+    return string.length;
+  }
+
+  const len = string.length;
+  let num = 0;
+  for (let i = 0; i < len; i++) {
+    if (prediction( string[i]) === true) {
+      num++;
+    }
+  }
+
+  return num;
 
   throw new Error('Please delete this line and implement the function');
 }
