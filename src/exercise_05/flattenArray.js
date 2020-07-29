8a4b8aab09b9300cb1ad316dde28e0a9987cb94a
chenzdido <chenzdido@gmail.com>
2020-07-30 00:01:23 +0800

    [zhe] feat:complete exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,19 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  let len = array.length;
+  let arr=[];
+  for (let i = 0; i < len; i++) {
+    if (array[i].length === undefined) {
+      arr.push(array[i]);
+    } else {
+      for (let j = 0; j < array[i].length; j++) {
+        arr.push(array[i][j]);
+      }
+    }
+  }
+  return arr;
 }
